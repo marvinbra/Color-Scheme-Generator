35506e7f65be8cb92d5961a2a8862a515a6b893d
marvinbra <marvinbopp37@gmail.com>
2023-10-02 13:03:29 +0200

    get hex value of color scheme
    js

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const getSchemeBtn = document.getElementById("get-scheme")
 const nmbDisplay =  document.querySelector(".nmb-display")
 const colorInput = document.getElementById("input-color")
 const colors = document.querySelector(".color")
+let newColor
 
 getSchemeBtn.addEventListener("click", () => {
-    console.log(colorInput.value)
-    nmbDisplay.textContent = `${colorInput.value}`
-    colors.style.backgroundColor = `${colorInput.value}`
-    console.log(nmbDisplay)
+    
+    const hexValue = colorInput.value.slice(1)
+    
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}`)
+        .then(res => res.json())
+        .then(data => {
+            newColor = data.colors
+            render()
+        })
 })
+
+function render() {
+    newColor.forEach( (color) => {
+        const index = newColor.indexOf(color)
+        console.log(color.hex.value)
+        nmbDisplay.textContent = `${colorInput.value}`
+        colors.style.backgroundColor = `${colorInput.value}`
+    })
+}
