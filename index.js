73f5cafc6c6be11621462841253cb7a21467d057
marvinbra <marvinbopp37@gmail.com>
2023-10-03 22:53:27 +0200

    select option for color scheme
    js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const getSchemeBtn = document.getElementById("get-scheme")
+const colorInput = document.getElementById("input-color")
 const hexList =  document.querySelectorAll(".nmb-display")
 const colorList = document.querySelectorAll(".color")
-const colorInput = document.getElementById("input-color")
+const optionsValue = document.querySelector("select")
 let newColor
 
 getSchemeBtn.addEventListener("click", () => {
     
     const hexValue = colorInput.value.slice(1)
     
-    fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}`)
-    .then(res => res.json())
-    .then(data => {
-        newColor = data.colors
-        render()
-    })
+    const selectedValue = optionsValue.value
     
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${selectedValue}`)
+        .then(res => res.json())
+        .then(data => {
+            newColor = data.colors
+            render()
+    }) 
 })
 
 function render() {
